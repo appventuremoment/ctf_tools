441b85b5e6a542ddee0066d517e30cf326984cc0
appeventuremoment <h2010027@nushigh.edu.sg>
2024-02-07 08:34:29 +0800

    another

--- XORExample.py
+++ XORExample.py
@@ -1,9 +1,10 @@
 import base64
 from pwn import *
 k1 = bytes.fromhex("0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104")
+print(xor(k1, b'crypto{'))
 
-#print(bytes.fromhex("0x22"))
-#print(bytes.fromhex(str(hex(2))))
+#find key is "myXORke......"
+#infer it is "myXORkey" repeated
 
 temp = xor(k1, b"myXORkeymyXORkeymyXORkeymyXORkeymyXORkeymyXORkeymyXORkeymyXORkeymyXORkeymyXORkeymyXORkey")
 print(temp)
